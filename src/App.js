ef1a0ab9cd81a32d60f36e4f68f5eb490407347e
damarowen <damarowen@gmail.com>
2021-01-28 20:08:30 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+
+
+
 function App() {
+  const [name, setName] = useState('damar')
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name}
         </p>
+        <button onClick={() => setName('jack')}>add me</button>
         <a
           className="App-link"
           href="https://reactjs.org"
