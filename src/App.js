c5b471254600b37e22e52ef33ac33ce20b0d105d
damarowen <damarowen@gmail.com>
2021-02-04 12:32:42 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
       .then(users => this.setState({ mobil: users }))
-
+       this.setState({ loading: true})
   }
 
   onChange = (e) => {
@@ -41,15 +41,14 @@ class App extends Component {
 
   render() {
 
-    const { mobil, searchField } = this.state
+    const { mobil, searchField, loading} = this.state
     const dataFilter = mobil.filter(x => x.name.toLowerCase().includes(searchField.toLowerCase()))
-  
-   
+ 
 
     return (
       <div className="App">
         <h1> Robot Database </h1>
-
+        {/* {this.state.loading ? <ReactBootStrap.Spinner animation='border'/> : ""} */}
         <SearchBox
           placeholder='cari apa'
           berubah={this.onChange}
